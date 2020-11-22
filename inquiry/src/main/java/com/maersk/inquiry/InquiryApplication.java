f106f19c1fa1f9beeacf60aad56ef2ef3ad7888e
srikant singh <srikantsingh51@gmail.com>
2020-11-22 18:59:50 +0530

    adding setup files.

---
+++ inquiry/src/main/java/com/maersk/inquiry/InquiryApplication.java
@@ -0,0 +1,13 @@
+package com.maersk.inquiry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InquiryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InquiryApplication.class, args);
+	}
+
+}
